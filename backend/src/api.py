62f1c95cd07c42b9f7681ee6b4427ab12e14175e
Paula Kedouk <paulakedouk@gmail.com>
2020-02-12 14:37:40 -0800

    Implement auth header and /drinks route

--- backend/src/api.py
+++ backend/src/api.py
@@ -16,7 +16,7 @@ CORS(app)
 !! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 '''
-# db_drop_and_create_all()
+db_drop_and_create_all()
 
 ## ROUTES
 '''
@@ -27,6 +27,14 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks', methods=['GET'])
+def get_drinks():
+    drinks = [drink.short() for drink in Drink.query.all()]
+    return jsonify({
+        'success': True,
+        'drinks': drinks
+    })
+
 
 
 '''
